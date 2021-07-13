02ea6d8bb84642b50acc33c411dcbc8331e54c14
Jerry Varughese <jerryjacob97@gmail.com>
2021-07-13 11:04:49 -0600

    code cleanup

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 import { annotate, annotationGroup } from 'https://unpkg.com/rough-notation?module';
 
 // highlights
-const h1 = annotate(document.querySelector('#highlight-1'), {type:'highlight', color:'#fbec5d', multiline: true, iterations: 1, animationDuration: 500})
-const h2 = annotate(document.querySelector('#highlight-2'), {type:'highlight', color:'#98fb98', multiline: true, iterations: 1, animationDuration: 500 })
-const h3 = annotate(document.querySelector('#highlight-3'), {type:'highlight', color:'#ace5ee', multiline: true, iterations: 1, animationDuration: 500 })
+const h1 = annotate(document.querySelector('#highlight-1'), {type:'highlight', color:'#fbec5d', multiline: true, iterations: 1, animationDuration: 600})
+const h2 = annotate(document.querySelector('#highlight-2'), {type:'highlight', color:'#98fb98', multiline: true, iterations: 1, animationDuration: 600 })
+const h3 = annotate(document.querySelector('#highlight-3'), {type:'highlight', color:'#ace5ee', multiline: true, iterations: 1, animationDuration: 600 })
 
 
 // underlines
-const u1 = annotate(document.querySelector('#underline-1'), {type: 'underline', animationDuration: 500 })
+const u1 = annotate(document.querySelector('#underline-1'), {type: 'underline', animationDuration: 600 })
 const u2 = annotate(document.querySelector('#projects-title'), {type: 'underline', animate:false })
 
 
